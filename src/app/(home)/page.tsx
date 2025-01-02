9df498f469d9742b2e24971a750659187ab7ba3f
Leandro Rodrigues <lj251004@gmail.com>
2025-01-02 20:53:26 -0300

    Remove unused imports and clean up the home page component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import Image from "next/image";
-
 import { api } from "@convex/_generated/api";
 import { useQuery } from "convex/react";
 
 import { FullscreenLoader } from "@/components/fullscreen-loader";
-import { Button } from "@/components/ui/button";
-import { SandwichIcon } from "lucide-react";
 import { Navbar } from "./navbar";
 
 export default function Page() {
